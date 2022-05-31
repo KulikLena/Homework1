eca0a76fcc7c31a2bdbd4367aeebe32b924d6f82
Lena Kulik <lenakulik6556@gmail.com>
2022-05-31 16:20:54 +0300

    console calculator

--- IT_academy_Python/BMICalculator.py
+++ IT_academy_Python/BMICalculator.py
@@ -5,8 +5,50 @@
 #Выводить результат в виде шкалы: Пример: Если результат 30
 #20======|============50
 #Если 40
-#20============|======50
+#20=====
+#from decimal import Decimal
 
-a = int(input("Введите ваш рост в сантиметрах: "))
-b = int(input())
-ptnt ("h")
+input_data = False
+while input_data==False: 
+ try:
+  a = int(input("Введите ваш рост в сантиметрах: "))
+  b = int(input("Введите ваш вес в килограммах: "))
+  input_data=True
+ except ValueError:
+    print ("Only whole numbers, without literal or decimals.")
+    input_data=False
+
+# пользователь вводит абышто
+if a<1 or a>210 or b<35 or b>400: 
+    a = int(input("Попробуйте еще раз. Ваш рост в сантиметрах:  "))
+    b = int(input("Попробуйте еще раз. Ваш вес в килограммах:  "))
+else: 
+     a=a
+     b=b
+
+index_bmi=int(b/(a/100)**2)
+#index_bmi= round(Decimal(b/(a/100)**2),0) 
+
+scale_size=37
+left_limit=13
+right_limit=50
+
+if index_bmi in range(left_limit,right_limit): 
+    left_interval=(index_bmi-left_limit-1)
+    right_interval=(scale_size-left_interval-2)
+    print (str(left_limit)+'='*left_interval+'|'+'='*right_interval+str(right_limit))
+else: 
+     print("You are propably dead")
+
+if index_bmi in range(left_limit, 18):
+       print("Your BMI index is "+str(index_bmi)+" and you are in the underweight range")
+elif index_bmi in range(18, 25):
+          print("Your BMI index is "+str(index_bmi)+" and you are in the healthy weight range")
+elif index_bmi in range(25, 30):
+          print("Your BMI index is "+str(index_bmi)+" and you are in the overweight range")
+elif index_bmi in range(30, 40):
+          print("Your BMI index is "+str(index_bmi)+" and you are in the obese range")
+else: 
+         print("You are propably dead")
+
+
